4eb1e33435b3ac5ccc54a0b6c0eaed4b8e376762
Jane Zdravevski <jane@zdravevski.pro>
2025-03-10 21:49:42 +0100

    styled the newsletter form, added toastify, and also form is NOT functional now, I just added 'method' and 'action' to the form, to showcase that you can actually send straight from the form, the request, with bunch of query params added automatically by the form, which is also by default GET request to the same URL you are on, with bunch of ?name='john'&lastName='doe'... etc. You can also use POST method there, with action='url to be sent to'

--- src/pages/Newsletter.jsx
+++ src/pages/Newsletter.jsx
@@ -1,8 +1,56 @@
 const Newsletter = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
     return (
-        <div>
-            <h2>Newsletter Page</h2>
-        </div>
+        <form className="form">
+            <h4 style={{ textAlign: "center", marginBottom: "2rem" }}>
+                our newsletter{" "}
+            </h4>
+            <div className="form-row">
+                <label htmlFor="name" className="form-label">
+                    first name
+                </label>
+                <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    className="form-input"
+                    defaultValue="john"
+                />
+            </div>
+            <div className="form-row">
+                <label htmlFor="lastName" className="form-label">
+                    last name
+                </label>
+                <input
+                    type="text"
+                    id="lastName"
+                    name="lastName"
+                    className="form-input"
+                    defaultValue="doe"
+                />
+            </div>
+            <div className="form-row">
+                <label htmlFor="email" className="form-label">
+                    email
+                </label>
+                <input
+                    type="text"
+                    id="email"
+                    name="email"
+                    className="form-input"
+                    defaultValue="test@test.com"
+                />
+            </div>
+            <button
+                type="submit"
+                className="btn btn-block"
+                style={{ marginTop: "0.5rem" }}
+            >
+                Submit
+            </button>
+        </form>
     );
 };
 
