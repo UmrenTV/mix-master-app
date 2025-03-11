83b559d6104fdb774dd6e24200084235fef0bf1a
Jane Zdravevski <jane@zdravevski.pro>
2025-03-11 11:32:15 +0100

    commented out the Query devtools from App.jsx so I can deploy to netlify and also check the public _redirects file, since without that for SPA if deployed to netlify the app won't work

--- src/App.jsx
+++ src/App.jsx
@@ -63,7 +63,7 @@ const App = () => {
     return (
         <QueryClientProvider client={queryClient}>
             <RouterProvider router={router} />
-            <ReactQueryDevtools initialIsOpen={false} />
+            {/*<ReactQueryDevtools initialIsOpen={false} /> */}
         </QueryClientProvider>
     );
 };
