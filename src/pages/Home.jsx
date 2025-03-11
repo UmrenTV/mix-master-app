23cd3c1d5afc5a435d1f7dff49a301fac1eb5dcb
Jane Zdravevski <jane@zdravevski.pro>
2025-03-11 10:58:38 +0100

    added the request in the loader hook, so the request that we make from the form, we can handle the url and extract the search query, etc + fixed the search form to handle requests using the Form component provided by the react-router-dom, without using hooks, methods, etc using the react router latest features

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,26 @@
 import { useEffect } from "react";
 import axios from "axios";
 import { useLoaderData } from "react-router-dom";
+import SearchForm from "../components/SearchForm";
 
 import CoctailList from "../components/CoctailList";
 
 const coctailSearchUrl =
     "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
 
-export const loader = async () => {
-    const searchTerm = "a";
+export const loader = async ({ request }) => {
+    const url = new URL(request.url);
+    const searchTerm = url.searchParams.get("search") || "vodka";
     const response = await axios.get(`${coctailSearchUrl}${searchTerm}`);
     return { drinks: response.data.drinks, searchTerm };
 };
 
 const Home = () => {
     const { drinks, searchTerm } = useLoaderData();
+
     return (
         <>
+            <SearchForm searchTerm={searchTerm} />
             <CoctailList drinks={drinks} />
         </>
     );
